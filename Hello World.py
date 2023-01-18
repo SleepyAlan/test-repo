97f590f5d7177457da5401742e41be42c6365eb3
SleepyAlan <110893287+SleepyAlan@users.noreply.github.com>
2023-01-18 14:50:42 -0700

    Update Hello World.py

--- Hello World.py
+++ Hello World.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+# Hello World.exe
+from tkinter import *
+from tkinter import ttk
+root = Tk()
+frm = ttk.Frame(root, padding=100)
+frm.grid()
+ttk.Label(frm, text="Hello World!").grid(column=0, row=0)
+ttk.Button(frm, text="DON'T PRESS", command=root.destroy).grid(column=1, row=0)
+ttk.Label(frm, text="By Team Sleepy").grid(column=1, row=1)
+root.mainloop()
